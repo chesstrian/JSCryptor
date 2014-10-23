80ff2365d38c070f59acb5f635be1af784e710aa
Christian Gutierrez <chesstrian@gmail.com>
2014-10-23 16:13:00 -0500

    New improvement on pbkdf2 method

--- examples.js
+++ examples.js
@@ -6,5 +6,6 @@ var password = 'myPassword';
 var b64string = "AwHsr+ZD87myaoHm51kZX96u4hhaTuLkEsHwpCRpDywMO1Moz35wdS6OuDgq+SIAK6BOSVKQFSbX/GiFSKhWNy1q94JidKc8hs581JwVJBrEEoxDaMwYE+a+sZeirThbfpup9WZQgp3XuZsGuZPGvy6CvHWt08vsxFAn9tiHW9EFVtdSK7kAGzpnx53OUSt451Jpy6lXl1TKek8m64RT4XPr";
 
 console.time('Decrypting');
-console.log("Result:", RNCryptor.Decrypt(b64string, password));
+var result = RNCryptor.Decrypt(b64string, password);
 console.timeEnd('Decrypting');
+console.log("Result:", result);

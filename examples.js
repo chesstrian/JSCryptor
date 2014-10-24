2a1b60f1dbabffbfdfe219ea7949adbdce3ef7a9
Christian Gutierrez <chesstrian@gmail.com>
2014-10-24 16:19:17 -0500

    Encryption support for Schema v3

--- examples.js
+++ examples.js
@@ -4,8 +4,19 @@ var RNCryptor = require('./index');
 
 var password = 'myPassword';
 var b64string = "AwHsr+ZD87myaoHm51kZX96u4hhaTuLkEsHwpCRpDywMO1Moz35wdS6OuDgq+SIAK6BOSVKQFSbX/GiFSKhWNy1q94JidKc8hs581JwVJBrEEoxDaMwYE+a+sZeirThbfpup9WZQgp3XuZsGuZPGvy6CvHWt08vsxFAn9tiHW9EFVtdSK7kAGzpnx53OUSt451Jpy6lXl1TKek8m64RT4XPr";
+var plain_text = 'There is no place like home';
+
+console.time('Encrypting');
+var encrypted = RNCryptor.Encrypt(plain_text, password);
+console.timeEnd('Encrypting');
+console.log("Result:", encrypted);
 
 console.time('Decrypting');
-var result = RNCryptor.Decrypt(b64string, password);
+var decrypted1 = RNCryptor.Decrypt(encrypted, password);
 console.timeEnd('Decrypting');
-console.log("Result:", result);
+console.log("Result:", decrypted1);
+
+console.time('Decrypting other');
+var decrypted2 = RNCryptor.Decrypt(b64string, password);
+console.timeEnd('Decrypting other');
+console.log("Result:", decrypted2);
